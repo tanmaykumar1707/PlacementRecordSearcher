3e083f1acb6a24067e697603baaf0ec64fc466df
Tanmay Kumar <tanmaykumar950@gmail,com>
2021-01-01 01:29:06 +0530

    First world

--- mca_app/views.py
+++ mca_app/views.py
@@ -1,16 +1,7 @@
 from django.shortcuts import render, HttpResponse
 import pandas as pd
 # Create your views here.
-sheetid = "1SWTx2PPEB5OxzjL50dd-SnQtLsWYnbC7pphqhkv7NUQ"
-sheet_name="NIT Agartala MCA Placement Record  2018-21 Batch"
-URL = 'https://docs.google.com/spreadsheets/d/{0}/gviz/tq?tqx=out:csv&sheet={1}'.format(sheetid,sheet_name)
-URL = URL.replace(" ", "%20")
-comp = pd.read_csv(URL)
-sheetid1="1FxDSMSavXbs-PPEUeF7JQEbJTHaceikXrltXeKG_Aok"
-sheet_name1="MCA_Candidates"
-URL1 = 'https://docs.google.com/spreadsheets/d/{0}/gviz/tq?tqx=out:csv&sheet={1}'.format(sheetid1,sheet_name1)
-URL1 = URL1.replace(" ", "%20")
-cand = pd.read_csv(URL1)
+
 
 
 def index(request):
@@ -22,6 +13,16 @@ def about(request):
 
 def data(request):
     conext = {}
+    sheetid = "1SWTx2PPEB5OxzjL50dd-SnQtLsWYnbC7pphqhkv7NUQ"
+    sheet_name="NIT Agartala MCA Placement Record  2018-21 Batch"
+    URL = 'https://docs.google.com/spreadsheets/d/{0}/gviz/tq?tqx=out:csv&sheet={1}'.format(sheetid,sheet_name)
+    URL = URL.replace(" ", "%20")
+    comp = pd.read_csv(URL)
+    sheetid1="1FxDSMSavXbs-PPEUeF7JQEbJTHaceikXrltXeKG_Aok"
+    sheet_name1="MCA_Candidates"
+    URL1 = 'https://docs.google.com/spreadsheets/d/{0}/gviz/tq?tqx=out:csv&sheet={1}'.format(sheetid1,sheet_name1)
+    URL1 = URL1.replace(" ", "%20")
+    cand = pd.read_csv(URL1)
     if request.method == "POST":
         print(request.POST["enroll"])
         t = request.POST["enroll"]
@@ -29,6 +30,7 @@ def data(request):
         comp_list=[]
         comp_list = cand.loc[cand.iloc[:,1] == t]["company_id"]
         eli_df = pd.DataFrame()
+        
         
         sheetid2="17j3A_KvZGhDNFy4C7mpfoTG9qRNclZ_6wnXo7yBkGPM"
         sheet_name2="MCA_Candidates"
